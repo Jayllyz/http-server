481f67a005a02593569f91bafad45ed4cbf017c5
Antony David <antonydavid945@gmail.com>
2024-06-22 17:38:44 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,9 +1,7 @@
-import * as net from "net";
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+import * as net from "node:net";
 
 const server = net.createServer((socket) => {
+	socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
 	socket.on("close", () => {
 		socket.end();
 	});
