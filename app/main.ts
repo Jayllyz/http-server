dac31346f21fcef15e3fff80877e65d8895ef364
Antony David <antonydavid945@gmail.com>
2024-06-22 17:52:57 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,10 +1,21 @@
 import * as net from "node:net";
 
 const server = net.createServer((socket) => {
-	socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
-	socket.on("close", () => {
-		socket.end();
-	});
+  socket.on("data", (data) => {
+    const buffer = data.toString();
+    if (buffer.includes("\r\n")) {
+      const [method, path] = buffer.split(" ");
+      console.log("method: ", method, "path: ", path);
+      if (path === "/") {
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      } else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
+    }
+  });
+  socket.on("close", () => {
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost");
