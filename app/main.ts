bca0b9886eaeb9aeb0eb4def57dc61818c40dad7
Antony David <antonydavid945@gmail.com>
2024-06-22 18:31:00 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import * as net from "node:net";
+import fs from "node:fs";
 
 function parseRequest(buffer: string): [string, string] {
   const [method, path] = buffer.split(" ");
@@ -9,19 +10,38 @@ function echoHandler(path: string): string {
   return path.slice(6);
 }
 
+function extractDirectory(): string {
+  const index = process.argv.indexOf("--directory");
+  return index > 0 && index + 1 < process.argv.length
+    ? process.argv[index + 1]
+    : "";
+}
+
 function handleGetRequest(path: string, buffer: string): string {
   switch (path) {
     case "/":
       return "HTTP/1.1 200 OK\r\n\r\n";
+    case "/user-agent": {
+      const userAgent = buffer.split("User-Agent: ")[1].split("\r\n")[0];
+      const length = userAgent.length;
+      return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${userAgent}\r\n\r\n`;
+    }
     case path.startsWith("/echo/") ? path : null: {
       const response = echoHandler(path);
       const length = response.length;
       return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${response}\r\n\r\n`;
     }
-    case "/user-agent": {
-      const userAgent = buffer.split("User-Agent: ")[1].split("\r\n")[0];
-      const length = userAgent.length;
-      return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${userAgent}\r\n\r\n`;
+    case path.startsWith("/files/") ? path : null: {
+      const fileName = path.slice(7);
+      const directory = extractDirectory();
+
+      if (!fs.existsSync(`${directory}/${fileName}`)) {
+        return "HTTP/1.1 404 Not Found\r\n\r\n";
+      }
+      const content = fs.readFileSync(`${directory}/${fileName}`, "utf-8");
+      const length = content.length;
+
+      return `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${length}\r\n\r\n${content}\r\n\r\n`;
     }
     default:
       return "HTTP/1.1 404 Not Found\r\n\r\n";
