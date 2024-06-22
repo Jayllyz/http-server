5bc7f0a423e7259ae4ed36c7d9069338d98d749e
Antony David <antonydavid945@gmail.com>
2024-06-22 18:14:06 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -9,6 +9,25 @@ function echoHandler(path: string): string {
   return path.slice(6);
 }
 
+function handleGetRequest(path: string, buffer: string): string {
+  switch (path) {
+    case "/":
+      return "HTTP/1.1 200 OK\r\n\r\n";
+    case path.startsWith("/echo") ? path : null: {
+      const response = echoHandler(path);
+      const length = response.length;
+      return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${response}\r\n\r\n`;
+    }
+    case "/user-agent": {
+      const userAgent = buffer.split("User-Agent: ")[1].split("\r\n")[0];
+      const length = userAgent.length;
+      return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${userAgent}\r\n\r\n`;
+    }
+    default:
+      return "HTTP/1.1 404 Not Found\r\n\r\n";
+  }
+}
+
 const server = net.createServer((socket) => {
   console.log("connected");
   socket.on("data", (data) => {
@@ -16,16 +35,14 @@ const server = net.createServer((socket) => {
     if (buffer.includes("\r\n")) {
       const [method, path] = parseRequest(buffer);
       console.log(method, path);
-      if (path === "/") {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
-      } else if (path.startsWith("/echo/")) {
-        const response = echoHandler(path);
-        const length = response.length;
-        socket.write(
-          `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${response}\r\n\r\n`,
-        );
-      } else {
-        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+
+      switch (method) {
+        case "GET": {
+          socket.write(handleGetRequest(path, buffer));
+          break;
+        }
+        default:
+          socket.write("HTTP/1.1 405 Method Not Allowed\r\n\r\n");
       }
     }
   });
