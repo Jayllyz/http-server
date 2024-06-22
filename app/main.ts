80f227969067cb503e1edda3abe3d69ef55230f2
Antony David <antonydavid945@gmail.com>
2024-06-22 18:03:31 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,29 @@
 import * as net from "node:net";
 
+function parseRequest(buffer: string): [string, string] {
+  const [method, path] = buffer.split(" ");
+  return [method, path];
+}
+
+function echoHandler(path: string): string {
+  return path.slice(6);
+}
+
 const server = net.createServer((socket) => {
+  console.log("connected");
   socket.on("data", (data) => {
     const buffer = data.toString();
     if (buffer.includes("\r\n")) {
-      const [method, path] = buffer.split(" ");
-      console.log("method: ", method, "path: ", path);
+      const [method, path] = parseRequest(buffer);
+      console.log(method, path);
       if (path === "/") {
         socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      } else if (path.startsWith("/echo/")) {
+        const response = echoHandler(path);
+        const length = response.length;
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${length}\r\n\r\n${response}\r\n\r\n`,
+        );
       } else {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
       }
